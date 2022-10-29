b0eb3846626a551d8de2287fc6efaff0552e0976
A-C0der <106011669+A-C0der@users.noreply.github.com>
2022-10-29 23:09:52 +1030

    Add files via upload

---
+++ gift_date/__manifest__.py
@@ -0,0 +1,15 @@
+# See LICENSE file for full copyright and licensing details.
+{
+	'name':"Gift Card Expired Date",
+	'version':'1.0',
+	'depends':['base','gift_card'],
+	'author' : 'Htet Arkar kyaw',
+	"license": "AGPL-3",
+	'description': """ 
+	  To work Gift Card Expired Date""",
+	'application':True,
+	'category':'Technical/Gift Card',
+	'website':'https://github.com/A-C0der/gift_date',
+	'sequence':-100, 
+	'data':['views/cron_giftcard.xml',],
+}
